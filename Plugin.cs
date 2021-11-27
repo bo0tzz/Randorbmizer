1736551e5a91fe623765f42278dbc05be2913b37
bo0tzz <btdewilligen@gmail.com>
2021-11-27 21:42:59 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace PeglinRandorbmizer
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Peglin.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        
+        private void Awake()
+        {
+            // Plugin startup logic
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
