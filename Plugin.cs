ea61e61dbb1fc411b18ff743443b6528dd057179
bo0tzz <btdewilligen@gmail.com>
2021-11-28 12:11:52 +0100

    Implement deck randomizer

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,8 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
 using HarmonyLib;
+using UnityEngine;
 
 namespace PeglinRandorbmizer
 {
@@ -9,11 +12,55 @@ namespace PeglinRandorbmizer
     {
         private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
         
+        private static System.Random Rand = new();
+        
+        public static GameObject[] OrbPool;
+
+        private static readonly string[] OrbBlacklist = {
+            "Orb",
+            "Orb Variant",
+            "Oreb-Lvl1",
+            "NavigationOrb",
+            "DisplayPrefab"
+        };
+
         private void Awake()
         {
-            // Plugin startup logic
+            GameObject[] orbs = Resources.LoadAll<GameObject>("Prefabs/Orbs/");
+            GameObject[] availableOrbs = orbs.Where(o => !OrbBlacklist.Contains(o.name)).ToArray();
+
+            OrbPool = availableOrbs;
+            
             harmony.PatchAll();
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+        }
+        
+        public static void RandomizeDeck(DeckManager deckManager)
+        {
+            List<GameObject> orbs = OrbPool.OrderBy(_ => Rand.Next()).Take(Rand.Next(2, 5)).ToList();
+            string pickedOrbNames = orbs.Select(GetName).Join();
+            Debug.Log("Randomized deck to orbs: " + pickedOrbNames);
+            deckManager.InstantiateDeck(orbs);
+        }
+    }
+
+    [HarmonyPatch(typeof(DeckManager), "ShuffleBattleDeck")]
+    public class DeckManagerPatch
+    {
+        public static bool Prefix(DeckManager __instance)
+        {
+            Plugin.RandomizeDeck(__instance);
+            __instance.ShuffleCompleteDeck();
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleController), "Start")]
+    public class BattleControllerPatch
+    {
+        public static void Prefix(DeckManager ____deckManager)
+        {
+            Plugin.RandomizeDeck(____deckManager);
         }
     }
 }
